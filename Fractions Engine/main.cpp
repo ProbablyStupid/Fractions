0c3ca9b83b3601a8fb0351571eb1deb18d6b8ca2
DevThomasCom <thomas.kaliwe@gmail.com>
2021-02-17 17:28:53 +0800

    initial commit

---
+++ Fractions Engine/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Fractions.h>
+
+int main()
+{
+	FractionsCallback();
+	return EXIT_SUCCESS;
+}
---
+++ Fractions/Fractions.cpp
@@ -0,0 +1,7 @@
+#include "Fractions.h"
+#include <iostream>
+
+void FractionsCallback()
+{
+	printf("\t --- FRACTIONS REPORTING ---\n");
+}
